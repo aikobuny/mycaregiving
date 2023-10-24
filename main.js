9327ccbf1543d406239a5df8c0da02c789d48bff
Aiko <aa.fakri143@gmail.com>
2023-10-24 12:59:59 +0800

    contact

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var activeDropdown = "";
 
-function includeHTML() {
+function includeHTML()
+{
     var z, i, elmnt, file, xhttp;
     /*loop through a collection of all HTML elements:*/
     z = document.getElementsByTagName("*");
@@ -28,7 +29,8 @@ function includeHTML() {
     }
 };
 
-function dropdown(id) {
+function dropdown(id)
+{
     console.log(id);
     let dropdownArray = ["OurStory", "Services"];
     for (let i=0; i<dropdownArray.length; i++) {
@@ -40,10 +42,51 @@ function dropdown(id) {
     } 
 }
 
-function goto(dir) {
+function goto(dir)
+{
     window.location.href = dir;
 }
 
-function scrollBottom() {
+function scrollBottom()
+{
     window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+}
+
+function getURLParam(sParam)
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) 
+        {
+            return sParameterName[1];
+        }
+    }
+}​
+
+function sendMessage(url)
+{
+    let name = getURLParam('name');
+    let email = getURLParam('email');
+    let phone = getURLParam('phone');
+    let message = getURLParam('message');
+
+    let output = 
+`
+Name: ${name}
+Email: ${email}
+Phone: ${phone}
+Message:
+\`\`\`${message}\`\`\`
+`
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", webHookURL, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({
+        'content': output,
+        'username':'Mycaregiving',
+    }));
 }
